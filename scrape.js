2a35c148f94563887593d9531e0305da80944cd9
haidertoqeer <haidertoqeer@gmail.com>
2024-10-31 14:37:16 +0500

    INIT

---
+++ scrape.js
@@ -0,0 +1,84 @@
+// import puppeteer from 'puppeteer';
+// import fs from 'fs';
+
+// const scrape = async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+
+//   const allBooks = [];
+//   let currentPage = 1;
+//   const maxPages = 10;
+
+//   while (currentPage <= maxPages) {
+//     const url = `https://books.toscrape.com/catalogue/page-${currentPage}.html`;
+
+//     await page.goto(url);
+
+//     const books = await page.evaluate(() => {
+//       const bookElements = document.querySelectorAll('.product_pod');
+//       return Array.from(bookElements).map((book) => {
+//         const title = book.querySelector('h3 a').getAttribute('title');
+//         const price = book.querySelector('.price_color').textContent;
+//         const stock = book.querySelector('.instock.availability')
+//           ? 'In Stock'
+//           : 'Out Of Stock';
+//         const rating = book
+//           .querySelector('.star-rating')
+//           .className.split(' ')[1];
+//         const link = book.querySelector('h3 a').getAttribute('href');
+
+//         return {
+//           title,
+//           price,
+//           stock,
+//           rating,
+//           link,
+//         };
+//       });
+//     });
+
+//     allBooks.push(...books);
+//     console.log(`Books on page ${currentPage}: `, books);
+//     currentPage++;
+//   }
+
+//   fs.writeFileSync('books.json', JSON.stringify(allBooks, null, 2));
+
+//   console.log('Data saved to books.json');
+
+//   await browser.close();
+// };
+
+// scrape();
+
+
+import puppeteer from 'puppeteer';
+import fs from 'fs';
+
+const scrape = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  const url = 'you-url-here-with-hee';
+  await page.goto(url);
+
+  const books = await page.evaluate(() => {
+    const bookElements = document.querySelectorAll('.service-item');
+    return Array.from(bookElements).map((book) => {
+      const title = book.querySelector('h6 strong').innerHTML;
+      const desc = book.querySelector('.hight-p').innerHTML
+      return {
+        title,
+        desc
+      };
+    });
+  });
+
+  fs.writeFileSync('books.json', JSON.stringify(books, null, 2));
+
+  console.log('Data saved to books.json');
+
+  await browser.close();
+};
+
+scrape();
